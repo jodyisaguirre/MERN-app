d3f688de11982c11837da6eed1fa5f38e5fc5741
Jody Isaguirre <jodyisaguirre@Jodys-MacBook-Air.local>
2020-02-25 18:11:04 -0700

    error handling

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const app = express();
 
 connectDB()
 
+app.use (express.json({extended:false}))
 
-app.get('/', (req, res) => res.send('API running'))
+app.get('/', (req, res) => res.send('API running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 const PORT  = process.env.PORT || 5000
 
